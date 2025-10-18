f25bddc448df0aa45847b73117a86e93f69648d8
filipgrzeskowiak <filip.grzeskowiak@anywhere.co>
2025-10-18 12:44:10 +0200

    Added missing Koin initialisation for iOS app

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+    
+    init() {
+        KoinApp.shared.startKoin()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
